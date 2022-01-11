b0a6788e25350b13831231f6a4bee38d7c35f7f3
kyk <naltlekwang@gmail.com>
2022-01-11 12:56:21 +0900

    Initial commit

---
+++ src/main/java/com/example/spingstart/SpingstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingstartApplication.class, args);
+	}
+
+}
